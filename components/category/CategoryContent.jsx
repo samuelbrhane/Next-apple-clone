22bc07e367c71b16a3316fee8b1d38038902513f
samuelberhane <1samibrhane@gmail.com>
2023-02-10 18:12:35 +0300

    Create top part of CategoryContent page for Mac.

--- components/category/CategoryContent.jsx
+++ components/category/CategoryContent.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { macData } from "../../utils/categories";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 const CategoryContent = ({ category }) => {
-  return <div>CategoryContent</div>;
+  const [categoryData, setCategoryData] = useState(null);
+  const [moving, setMoving] = useState(false);
+  const categoryRef = useRef(null);
+
+  useEffect(() => {
+    switch (category) {
+      case "Mac":
+        setCategoryData(macData);
+        break;
+
+      default:
+        break;
+    }
+  }, [category]);
+
+  const handleScroll = (direction) => {
+    setMoving(true);
+    if (categoryRef.current) {
+      const { scrollLeft, clientWidth } = categoryRef.current;
+
+      const scrollDirection =
+        direction === "right"
+          ? scrollLeft + clientWidth
+          : scrollLeft - clientWidth;
+
+      categoryRef.current.scrollTo({
+        left: scrollDirection,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <main className="max-w-7xl mx-auto">
+      <div className="relative group">
+        {moving && (
+          <AiOutlineLeft
+            className="absolute cursor-pointer left-0 opacity-0 group-hover:opacity-100  top-[50%] translate-y-[-50%] text-lg bg-[#d6ddf4] h-full z-20"
+            onClick={() => handleScroll("left")}
+          />
+        )}
+        <div
+          className="flex items-center gap-4 px-4 overflow-y-scroll scrollbar-hide justify-between"
+          ref={categoryRef}
+        >
+          {categoryData?.map((data, index) => (
+            <div key={index}>
+              <div className="w-[100px] h-[70px] mt-2">
+                <img
+                  className="w-full h-full"
+                  src={data.image}
+                  alt="itemImage"
+                />
+              </div>
+
+              <p className="mt-4 text-center text-sm">{data.name}</p>
+            </div>
+          ))}
+        </div>
+        <AiOutlineRight
+          className="absolute cursor-pointer right-0 top-[50%] opacity-0 group-hover:opacity-100  translate-y-[-50%] text-lg bg-[#d6ddf4] h-full z-20"
+          onClick={() => handleScroll("right")}
+        />
+      </div>
+    </main>
+  );
 };
 
 export default CategoryContent;
---
+++ utils/categories.js
@@ -0,0 +1,40 @@
+const macData = [
+  {
+    name: "MacBook Air",
+    image: "/img/mac/macbookair.svg",
+  },
+  {
+    name: "MacBook Pro",
+    image: "/img/mac/macbookpro.svg",
+  },
+  {
+    name: 'iMac 24"',
+    image: "/img/mac/imac.svg",
+  },
+  {
+    name: "Mac mini",
+    image: "/img/mac/mac_mini.svg",
+  },
+  {
+    name: "Mac Studio",
+    image: "/img/mac/mac_studio.svg",
+  },
+  {
+    name: "Mac Pro",
+    image: "/img/mac/mac_pro.svg",
+  },
+  {
+    name: "Compare",
+    image: "/img/mac/mac_compare.svg",
+  },
+  {
+    name: "Display",
+    image: "/img/mac/displays.svg",
+  },
+  {
+    name: "Accessories",
+    image: "/img/mac/accessories.svg",
+  },
+];
+
+export { macData };
