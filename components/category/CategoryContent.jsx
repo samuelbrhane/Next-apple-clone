6d9e2168a83f8b84a61954ab7c81254499bd1fa0
samuelberhane <1samibrhane@gmail.com>
2023-02-11 09:24:44 +0300

    Create data for categoryImage content.

--- components/category/CategoryContent.jsx
+++ components/category/CategoryContent.jsx
@@ -1,34 +1,33 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import {
-  macData,
-  watchData,
-  iPadData,
-  iPhoneData,
-  airPodsData,
+  macItems,
+  watchItems,
+  iPadItems,
+  iPhoneItems,
+  airPodsItems,
 } from "../../utils/categories";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
+import CategoryItems from "./CategoryItems";
 
 const CategoryContent = ({ category }) => {
   const [categoryData, setCategoryData] = useState(null);
-  const [moving, setMoving] = useState(false);
-  const categoryRef = useRef(null);
 
   useEffect(() => {
     switch (category) {
       case "mac":
-        setCategoryData(macData);
+        setCategoryData(macItems);
         break;
       case "iPhone":
-        setCategoryData(iPhoneData);
+        setCategoryData(iPhoneItems);
         break;
       case "iPad":
-        setCategoryData(iPadData);
+        setCategoryData(iPadItems);
         break;
       case "watch":
-        setCategoryData(watchData);
+        setCategoryData(watchItems);
         break;
       case "airPods":
-        setCategoryData(airPodsData);
+        setCategoryData(airPodsItems);
         break;
 
       default:
@@ -36,58 +35,15 @@ const CategoryContent = ({ category }) => {
     }
   }, [category]);
 
-  const handleScroll = (direction) => {
-    setMoving(true);
-    if (categoryRef.current) {
-      const { scrollLeft, clientWidth } = categoryRef.current;
-
-      const scrollDirection =
-        direction === "right"
-          ? scrollLeft + clientWidth
-          : scrollLeft - clientWidth;
-
-      categoryRef.current.scrollTo({
-        left: scrollDirection,
-        behavior: "smooth",
-      });
-    }
-  };
-
   return (
     <main>
-      <div
-        className={`relative flex justify-center group ${
-          category === "watch" && "bg-black"
-        } ${category === "watch" && "text-white"}`}
-      >
-        {moving && (
-          <AiOutlineLeft
-            className="absolute cursor-pointer left-0 opacity-0 group-hover:opacity-100  top-[50%] translate-y-[-50%] text-lg bg-[#d6ddf4] h-full z-20"
-            onClick={() => handleScroll("left")}
-          />
-        )}
-        <div
-          className="flex items-center gap-4 px-4 overflow-y-scroll scrollbar-hide"
-          ref={categoryRef}
-        >
-          {categoryData?.map((data, index) => (
-            <div key={index}>
-              <div className="w-[100px] h-[70px] mt-2">
-                <img
-                  className="w-full h-full"
-                  src={data.image}
-                  alt="itemImage"
-                />
-              </div>
-
-              <p className="mt-4 text-center text-[12px]">{data.name}</p>
-            </div>
-          ))}
-        </div>
-        <AiOutlineRight
-          className="absolute cursor-pointer right-0 top-[50%] opacity-0 group-hover:opacity-100  translate-y-[-50%] text-lg bg-[#d6ddf4] h-full z-20"
-          onClick={() => handleScroll("right")}
-        />
+      <CategoryItems categoryData={categoryData} category={category} />
+      <div className="flex justify-center px-4 md:px-8 py-2 bg-[#d1d5d3] text-sm font-light">
+        <p className="text-center">
+          Get 3% Daily Cash back with Apple Card. And pay for your new Apple
+          product over 12 months, interest&#8211;free when you choose to check
+          out with Apple Card Monthly Installments.
+        </p>
       </div>
     </main>
   );
