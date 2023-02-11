77c939b7ea3006f14e0b08ade02bce29ef6e5f88
samuelberhane <1samibrhane@gmail.com>
2023-02-11 11:14:32 +0300

    Add images for detail description of each category items.

---
+++ utils/categoryDesc.js
@@ -0,0 +1,46 @@
+const macDescData = [
+  {
+    image: "",
+    name: "",
+    desc: "",
+    price: "",
+    details: [
+      {
+        img: "",
+        description: "",
+      },
+      {
+        title: "",
+        description: "",
+      },
+    ],
+  },
+];
+const watchDescData = [
+  {
+    image: "",
+  },
+];
+const iPadDescData = [
+  {
+    image: "",
+  },
+];
+const iPhoneDescData = [
+  {
+    image: "",
+  },
+];
+const airPodsDescData = [
+  {
+    image: "",
+  },
+];
+
+export {
+  macDescData,
+  watchDescData,
+  iPadDescData,
+  iPhoneDescData,
+  airPodsDescData,
+};
