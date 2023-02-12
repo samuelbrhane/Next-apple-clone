a5a4427da865ac4c232e67cacec67ffa2641a2f0
samuelberhane <1samibrhane@gmail.com>
2023-02-12 10:43:29 +0300

    create data for bag recommendation items.

---
+++ utils/bagRecommendation.js
@@ -0,0 +1,54 @@
+const recommendation = [
+  {
+    name: "/img/recomm/smartiPad.jpg",
+    image: "Smart Folio for iPad (10th generation) - White",
+    price: "79.00",
+  },
+  {
+    name: "/img/recomm/magiciPad.jpg",
+    image: "Magic Keyboard Folio for iPad (10th generation) - White",
+    price: "249.00",
+  },
+  {
+    name: "/img/recomm/pencil.jpg",
+    image: "Logitech Crayon for iPad",
+    price: "69.95",
+  },
+  {
+    name: "/img/recomm/headadapter.jpg",
+    image: "USB-C to 3.5 mm Headphone Jack Adapter",
+    price: "9.00",
+  },
+  {
+    name: "/img/recomm/smartiPad.jpg",
+    image: "Smart Folio for iPad (10th generation) - White",
+    price: "79.00",
+  },
+  {
+    name: "/img/poweradapter.jpg",
+    image: "20W USB-C Power Adapter",
+    price: "19.00",
+  },
+  {
+    name: "/img/recomm/cable.jpg",
+    image: "USB-C Charge Cable(2 m)",
+    price: "19.00",
+  },
+  {
+    name: "/img/recomm/reader.jpg",
+    image: "USB-C to SD Card Reader",
+    price: "39.00",
+  },
+  {
+    name: "/img/recomm/air2nd.jpg",
+    image: "AirPods Pro (2nd generation)",
+    price: "249.00",
+  },
+  {
+    name: "/img/recomm/air3rd.jpg",
+    image: "AirPods (3rd generation) with MagSage Case",
+    price: "179.00",
+  },
+];
+
+export { recommendation };
